110f2894a021d03ddf649de968692b2b046848c6
Xiaoyang Liu <siujoeng.lau@gmail.com>
2022-09-27 14:44:55 -0700

    feat: implement the 'process' module
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 mod cli;
 mod device;
 mod mount;
+mod process;
---
+++ src/process.rs
@@ -0,0 +1,34 @@
+use nix::{sched::{CloneFlags, clone}, unistd::Pid};
+use oci_spec::runtime::{LinuxNamespace, LinuxNamespaceType};
+use std::error::Error;
+
+fn namespace_to_clone_flag(namespace: &LinuxNamespace) -> CloneFlags {
+    match namespace.typ() {
+        LinuxNamespaceType::Mount => CloneFlags::CLONE_NEWNS,
+        LinuxNamespaceType::Cgroup => CloneFlags::CLONE_NEWCGROUP,
+        LinuxNamespaceType::Uts => CloneFlags::CLONE_NEWUTS,
+        LinuxNamespaceType::Ipc => CloneFlags::CLONE_NEWIPC,
+        LinuxNamespaceType::User => CloneFlags::CLONE_NEWUSER,
+        LinuxNamespaceType::Pid => CloneFlags::CLONE_NEWPID,
+        LinuxNamespaceType::Network => CloneFlags::CLONE_NEWNET,
+    }
+}
+
+/// `clone_process` creates a child process that invokes `function` in seperated
+/// Linux namespaces specified in `namespace_list`
+pub fn clone_process(
+    function: impl Fn() -> isize,
+    namespace_list: &Vec<LinuxNamespace>,
+) -> Result<Pid, Box<dyn Error>> {
+    let clone_flags = namespace_list
+        .iter()
+        .map(|namespace| namespace_to_clone_flag(namespace))
+        .reduce(|flag_1, flag_2| flag_1 | flag_2 )
+        .unwrap_or(CloneFlags::empty());
+
+    const STACK_SIZE: usize = 4 * 1024 * 1024;
+    let ref mut stack: [u8; STACK_SIZE] = [0; STACK_SIZE];
+
+    let pid = clone(Box::new(function), stack, clone_flags, None)?;
+    Ok(pid)
+}
